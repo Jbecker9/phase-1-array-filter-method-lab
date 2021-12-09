d21b823ce4b386990d19972f195ababcbff0061b
Jared <jbeckersalax9@aol.com>
2021-12-09 18:06:25 -0500

    Done.

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ function findMatching(drivers, index){
     return drivers.filter(x => x.toLowerCase() === index.toLowerCase())
 }
 
-// const findMatching = function(arr){
-//     for (const drivers of arr) {
-//         if (findMatching(drivers)){
-//             return arr.drivers
-//         }
-//     }
-// }
-// findMatching(drivers, drivers.filter(drivers === 'Bobby' && drivers === 'Sammy'))
+function fuzzyMatch(drivers, index){
+    return drivers.filter(x => index === 'Sa')
+}
