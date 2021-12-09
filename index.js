2593217c2a33d17a66b634d8d3199b6338046a1d
Jared <jbeckersalax9@aol.com>
2021-12-09 16:40:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function findMatching(drivers, `Bobby` && 'Sammy')
 
+// const findMatching = function(arr){
+//     for (const drivers of arr) {
+//         if (findMatching(drivers)){
+//             return arr.drivers
+//         }
+//     }
+// }
+// findMatching(drivers, drivers.filter(drivers === 'Bobby' && drivers === 'Sammy'))
