c2a5d23ba91d3e5e3a17ec74e9e3f05139d5b421
Jared <jbeckersalax9@aol.com>
2021-12-09 16:13:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+const findMatching = function(arr){
+    for (const drivers of arr) {
+        if (findMatching(drivers)){
+            return arr.drivers
+        }
+    }
+}
+findMatching(drivers, function(drivers){
+    if(drivers === 'Bobby' && drivers === 'Sammy'){
+    return drivers
+    }
+})
