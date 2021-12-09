9c13ccf7f9210f9b877851e0f40ced13a3431d59
Jared <jbeckersalax9@aol.com>
2021-12-09 17:11:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function findMatching(drivers, index){
-    return drivers.filter('Bobby' && 'Sammy')
+    return drivers.filter(x => x.toLowerCase() === index.toLowerCase())
 }
 
 // const findMatching = function(arr){
